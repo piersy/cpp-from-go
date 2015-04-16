28adc333a0037df4b17594a77ba3c5bb3d50e498
piersy <pierspowlesland@gmail.com>
2015-04-16 15:08:18 +0100

    Got the example working to my satisfaction

--- Main.go
+++ Main.go
@@ -23,12 +23,17 @@
 //// Output: 42
 //}
 
+/*
+Untill the fix for https://github.com/golang/go/issues/5428 is released we may have to
+just put the shard library in the root directory of the project
+ the fix will allow use of relative paths in the #cgo LDFLAGS variable via the use of ${SRCDIR}
+*/
 
 package main
 import "fmt"
 /*
-#cgo CFLAGS: -I./c
-#cgo LDFLAGS: -L./c -lMyMain_c
+#cgo CFLAGS: -I./c -I.
+#cgo LDFLAGS: -L. -lMyMain_c
 #include <MyMain_c.h>
 */
 import "C"
