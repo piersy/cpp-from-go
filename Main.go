e377faaaa0c0da40a605d13f3e61659d6e275f5a
piersy <pierspowlesland@gmail.com>
2015-04-16 12:15:21 +0100

    Calling c++ fom go now working, although wierdness in finding the shared lib

--- Main.go
+++ Main.go
@@ -10,28 +10,30 @@
 //    fmt.Println(int(C.random()))
 //}
 
-package main
-import "fmt"
-// int fortytwo()
-// {
-//	    return 42;
-// }
-import "C"
-
-func main() {
-fmt.Println(int(C.fortytwo()))
-// Output: 42
-}
-
-
 //package main
 //import "fmt"
-///*
-//#cgo CFLAGS: -I.
-//#include<add.h>
-//*/
+//// int fortytwo()
+//// {
+////	    return 42;
+//// }
 //import "C"
 //
 //func main() {
-//    fmt.Println(C.add(5, 4))
+//fmt.Println(int(C.fortytwo()))
+//// Output: 42
 //}
+
+
+package main
+import "fmt"
+/*
+#cgo CFLAGS: -I./c
+#cgo LDFLAGS: -L./c -lMyMain_c
+#include <MyMain_c.h>
+*/
+import "C"
+//This worked only after i copied libMyMain.so into /usr/lib ??
+func main() {
+    C.pain(C.int(5), nil)
+    fmt.Println(5)
+}
---
+++ c/MyMain_c.h
@@ -0,0 +1,6 @@
+#ifndef __MyMain_C_H
+#define __MyMain_C_H
+
+int pain(int argc, char* argv[]);
+
+#endif
